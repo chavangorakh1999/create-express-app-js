e79783ba33fedfe0ce94a12ae4a4348d2ecd4e0a
chavangorakh1999 <chavangorakh19999@gmail.com>
2024-03-31 22:11:10 +0530

    Added mock file system

--- bin/express-app.js
+++ bin/express-app.js
@@ -1,46 +1,52 @@
 #!/usr/bin/env node
 
-const { program } = require('commander');
-const fs = require('fs');
-const path = require('path');
-const { execSync } = require('child_process');
+const { program } = require("commander");
+const fs = require("fs");
+const path = require("path");
+const { execSync } = require("child_process");
 
+// Parse command-line arguments
 program
-  .version('1.0.0')
-  .description('Generate a new Express application')
-  .option('-n, --name <name>', 'Application name')
-  .option('-d, --directory <directory>', 'Directory to create the application in')
+  .version("1.0.4")
+  .description("Generate a new Express application")
+  .option(
+    "-d, --directory <directory>",
+    "Directory to create the application in"
+  )
   .parse(process.argv);
 
 const run = async () => {
-  const { name: appName, directory: appDirectory } = program.opts();
+  let appName = process.argv[2];
 
-  const inquirer = await import('inquirer');
-  const chalk = await import('chalk');
-
-  let appNamePrompt;
-  if (!appName) {
-    appNamePrompt = await inquirer.default.prompt([
-      {
-        type: 'input',
-        name: 'name',
-        message: 'Enter the name of your Express application:',
-        validate: (input) => {
-          if (!input) {
-            return 'Please enter a name for your application.';
-          }
-          return true;
-        },
-      },
-    ]);
+  // Check if the first argument is a dot (indicating current directory)
+  if (appName === "." && process.argv.length === 3) {
+    // Get the base name of the current working directory
+    appName = path.basename(process.cwd());
+  } else if (!appName || process.argv.length !== 3) {
+    console.error("Error: Application name must be provided.");
+    process.exit(1);
   }
 
-  const name = appNamePrompt?.name || appName;
-  const templatePath = path.join(__dirname, '../templates');
-  const appPath = path.join(process.cwd(), appDirectory || name);
+  const inquirer = await import("inquirer");
+  const chalk = await import("chalk");
+
+  const authorPrompt = await inquirer.default.prompt([
+    {
+      type: "input",
+      name: "author",
+      message: "Enter the name of the author:",
+    },
+  ]);
+
+  const author = authorPrompt.author || "";
+
+  const templatePath = path.join(__dirname, "../app_template");
+  const appPath = path.join(process.cwd(), program.opts().directory || appName);
 
   if (fs.existsSync(appPath)) {
-    console.error(chalk.default.red(`Error: Directory ${appPath} already exists.`));
+    console.error(
+      chalk.default.red(`Error: Directory ${appPath} already exists.`)
+    );
     process.exit(1);
   }
 
@@ -62,25 +68,44 @@ const run = async () => {
   process.chdir(appPath);
 
   const packageJson = {
-    name,
-    version: '1.0.0',
-    description: 'Express application generated with create-express-app-js',
-    main: 'index.js',
+    name: appName,
+    version: "1.0.0",
+    description: "Express application generated with create-express-app-js",
+    main: "index.js",
     scripts: {
-      start: 'node index.js',
+      start: "node index.js",
     },
-    keywords: ['express', 'generator', 'cli', 'template'],
-    author: '',
-    license: 'MIT',
+    keywords: ["express", "generator", "cli", "template"],
+    author,
+    license: "MIT",
     dependencies: {
-      express: '^4.17.1',
+      "body-parser": "^1.20.2",
+      cheerio: "^1.0.0-rc.12",
+      cors: "^2.8.5",
+      dotenv: "^16.4.5",
+      express: "^4.19.2",
+      path: "^0.12.7",
+      jsonwebtoken: "^9.0.2",
+      mongoose: "^8.2.4",
     },
   };
 
-  fs.writeFileSync(path.join(appPath, 'package.json'), JSON.stringify(packageJson, null, 2));
+  fs.writeFileSync(
+    path.join(appPath, "package.json"),
+    JSON.stringify(packageJson, null, 2)
+  );
+  console.log(
+    chalk.default.cyan(`\nExpress application '${appName}' created'`)
+  );
+  console.log(chalk.default.cyan(`\nInstalling node modules..`));
+  // Run npm install in the newly created directory
+  execSync("npm install", { stdio: "inherit", cwd: appPath });
 
-  console.log(chalk.default.green(`Success! Express application '${name}' created in '${appPath}'`));
-  
+  console.log(
+    chalk.default.green(`\nExpress server is ready for action!\n`),
+    "\n",
+    chalk.default.cyan(`To start the server run: \n \n npm start \n \n`)
+  );
 };
 
 run();
